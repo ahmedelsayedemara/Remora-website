491fd6e6467350aecf4b96099bb82e679267e596
Alaa ElHamouly <AElHamouly@sure.com.sa>
2023-05-28 02:18:37 +0300

    Finalize Blog details

---
+++ src/components/Shared/BreadCrumb/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="breadcrumb">
+    <router-link to="/">
+        <p>{{$t("GLOBAL.HOME")}}</p> 
+    </router-link>
+    <span>></span>
+    <router-link to="/blog">
+        <p>{{ $t("BLOG.BLOG") }}</p> 
+    </router-link>
+    <span>></span>
+    <p class="current">{{ current }}</p>
+  </div>
+</template>
+<script>
+export default {
+    props: {
+        current: {
+            type: String,
+            default: ""
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
