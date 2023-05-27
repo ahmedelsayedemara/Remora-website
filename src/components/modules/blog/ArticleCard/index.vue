7effc9e06ac7f6ccee8daefe81267d30b225541f
Alaa ElHamouly <AElHamouly@sure.com.sa>
2023-05-27 10:02:36 +0300

    blog part one

---
+++ src/components/modules/blog/ArticleCard/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="article">
+        article card is under construction...
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+@import './index.scss'
+</style>
--- src/views/application/blog/list/index.vue
+++ src/views/application/blog/list/index.vue
@@ -13,13 +13,30 @@
           :options="categories"
         />
       </div>
+      <div class="blog__content">
+        <b-container>
+          <h3 class="fs-40">{{ $t("BLOG.NEWS_AND_ARTICLES") }}</h3>
+          <p class="fs-18">{{ $t("BLOG.SUBTITLE") }}</p>
+          <div class="blog__filter">
+            <div v-for="(item, idx) in filterItems" :key="idx" class="blog__filter-item">
+              <span class="fs-12">{{ item.name }}</span>
+            </div>
+          </div>
+          <div class="blog__artcles">
+            <ArticleCard />
+          </div>
+        </b-container>
+      </div>
     </DefaultLayout>
   </div>
 </template>
 
 <script>
+import ArticleCard from "@/components/modules/blog/ArticleCard/index.vue"
 export default {
-  components: {},
+  components: {
+    ArticleCard
+  },
   data() {
     return {
       formValues: {
@@ -37,7 +54,121 @@ export default {
         {
           id: 3,
           name: "category_item_three"
+        }
+      ],
+      filterItems: [
+        {
+          id: 1,
+          name: "AMA"
         },
+        {
+          id: 2,
+          name: "Africa"
+        },
+        {
+          id: 3,
+          name: "Altcoin"
+        },
+        {
+          id: 4,
+          name: "Altcoin"
+        },
+        {
+          id: 5,
+          name: "Anti-Fud"
+        },
+        {
+          id: 6,
+          name: "Anti-Fud"
+        },
+        {
+          id: 7,
+          name: "BTC"
+        },
+        {
+          id: 8,
+          name: "Bitcoin"
+        },
+        {
+          id: 9,
+          name: "Blockchain"
+        },
+        {
+          id: 10,
+          name: "Buy Crypto"
+        },
+        {
+          id: 11,
+          name: "Consensus"
+        },
+        {
+          id: 12,
+          name: "Crypto"
+        },
+        {
+          id: 13,
+          name: "Crypto Gem"
+        },
+        {
+          id: 14,
+          name: "DeFi"
+        },
+        {
+          id: 15,
+          name: "ETH Merge"
+        },
+        {
+          id: 16,
+          name: "Ethereum"
+        },
+        {
+          id: 17,
+          name: "KCC"
+        },
+        {
+          id: 18,
+          name: "KuCoin Community"
+        },
+        {
+          id: 19,
+          name: "Mining"
+        },
+        {
+          id: 20,
+          name: "NFT"
+        },
+        {
+          id: 21,
+          name: "NFTs"
+        },
+        {
+          id: 22,
+          name: "Report"
+        },
+        {
+          id: 23,
+          name: "Secuirty"
+        },
+        {
+          id: 24,
+          name: "Security"
+        },
+        {
+          id: 25,
+          name: "Technical Analysis"
+        },
+        {
+          id: 26,
+          name: "Trading"
+        },
+        {
+          id: 27,
+          name: "Trading Bot"
+        },
+        {
+          id: 28,
+          name: "Tutorial"
+        }
       ]
     }
   },
