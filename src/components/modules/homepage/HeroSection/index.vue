a3682a94a75e922e8b8ef576cd472fc8b9ed0810
mahmoud amer <mamer@sure.com.sa>
2023-07-28 23:09:59 +0300

    loader + muliple comments

--- src/components/modules/homepage/HeroSection/index.vue
+++ src/components/modules/homepage/HeroSection/index.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="hero-section">
     <div class="container-fluid">
-    <b-row>
-      <b-col cols="12" lg="6" class="text-container">
-        <h1 class="fs-56">{{ data.title }}</h1>
-        <p class="fs-20">{{ data.text }}</p>
-        <p class="bold fs-20">{{ data.label }}</p>
-        <div class="btn-container mt-5">
-          <Button variant="primary">
-            {{ data.btnText }}
-          </Button>
-        </div>
-      </b-col>
-      <b-col cols="12" lg="6" class="img-container">
-        <img :src="require(`@/assets/images/${data.img.url}`)" :alt="data.img.alt" />
-        <div class="mask d-none d-xl-block"></div>
-      </b-col>
-    </b-row>
-  </div>
+      <b-row>
+        <b-col cols="12" lg="6" class="text-container">
+          <h1 class="fs-56">{{ data.title }}</h1>
+          <p class="fs-20">{{ data.text }}</p>
+          <p class="bold fs-20">{{ data.label }}</p>
+          <div class="btn-container mt-5">
+            <Button variant="primary" @click="$emit('scrollToJoinNow')">
+              {{ data.btnText }}
+            </Button>
+          </div>
+        </b-col>
+        <b-col cols="12" lg="6" class="img-container">
+          <img :src="require(`@/assets/images/${data.img.url}`)" :alt="data.img.alt" />
+          <div class="mask d-none d-xl-block"></div>
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 
--- src/views/application/blog/details/index.vue
+++ src/views/application/blog/details/index.vue
@@ -1,6 +1,7 @@
 <template>
   <DefaultLayout>
-    <div class="blog-details">
+    <PageLoader v-if="loading" />
+    <div v-else class="blog-details">
       <b-container>
         <b-row>
           <b-col lg="12">
@@ -8,72 +9,180 @@
           </b-col>
         </b-row>
         <b-row>
-          <b-col lg="8">
+          <b-col lg="12">
             <div class="blog-details__article">
               <div class="blog-details__title fs-40">
                 {{ article.title }}
               </div>
               <div class="blog-details__date fs-16">{{ article.date }}</div>
-              <div class="blog-details__tags">
+              <!-- <div class="blog-details__tags">
                 <div v-for="tag in article.tags" :key="tag.id" class="blog-details__tag">
                   <span class="fs-12">{{ tag.name }}</span>
                 </div>
-              </div>
+              </div> -->
               <div class="blog-details__image">
                 <img :src="require(`@/assets/images/${article.image}`)" alt="article-image" />
               </div>
               <div class="blog-details__content">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur. Vitae vitae ut viverra amet. Integer
-                  posuere mattis eget cursus quam ultricies volutpat integer elit. Tincidunt eu
-                  dolor nec orci adipiscing. Blandit enim id faucibus leo massa ac amet magna. Id
-                  vitae sed in faucibus.
-                </p>
-                <h3>Ultricies lectus phasellus arcu sagittis in</h3>
-                <p>
-                  >Ultrices ut amet risus duis id tellus. Tellus id pharetra leo sit volutpat.
-                  Tortor sed nibh mi quisque vitae risus sed ut. Eget leo dignissim eget amet in
-                  tempus. Aliquet mi ultrices magna viverra montes semper. Pharetra quam non mi
-                  malesuada augue vestibulum mauris nam rhoncus. Elementum placerat ut posuere eget.
-                  Sed mi faucibus arcu amet sit ante amet habitasse amet. Nullam faucibus diam
-                  faucibus euismod feugiat vel amet mauris. Sem turpis massa sollicitudin pulvinar
-                  sed habitasse aliquam massa in. Tristique sed consequat venenatis adipiscing
-                  parturient. Ultricies lectus phasellus arcu sagittis in. Pellentesque vitae sed
-                  ultrices arcu vel viverra at. Et leo enim amet suspendisse in mauris et tortor
-                  semper. Accumsan facilisis phasellus purus elementum quisque adipiscing a neque
-                  aliquet. Sapien neque velit posuere eu mattis aliquam commodo. Platea proin nulla
-                  nulla condimentum nunc cum. Tellus nunc vel magna et mollis consectetur.
-                  Adipiscing urna fringilla nunc cras. Sapien convallis dignissim a gravida fames ut
-                  pretium proin. Tristique egestas sagittis ut phasellus egestas lacinia. A facilisi
-                  elit nisl massa est elementum augue ullamcorper lorem. Fringilla velit nisi ac
-                  adipiscing.
-                </p>
-                <h3>Tristique sed consequat venenatis adipiscing parturient?</h3>
-                <p>
-                  Risus pulvinar dictumst porta est massa cursus proin nulla. Vitae posuere
-                  tincidunt orci eget. At ut porttitor mauris id sagittis duis. Interdum tincidunt
-                  ut morbi aenean rhoncus libero porta porttitor. Ut fames proin imperdiet pretium
-                  sed. A eu netus elit dignissim tellus consectetur ultrices varius. Hendrerit
-                  viverra lectus pretium pretium cras viverra. Ornare urna dolor tortor scelerisque
-                  ac urna suspendisse. Mollis ut arcu posuere id volutpat. Fermentum faucibus massa
-                  sed porta cursus. Accumsan vel euismod hac orci volutpat facilisis. Pretium
-                  penatibus libero ullamcorper integer magna dui duis et.
-                </p>
-                <h3>Odio lectus nisi ut id lectus facilisis non</h3>
-                <p>
-                  Odio tincidunt enim pellentesque pretium congue ipsum imperdiet sem. Congue
-                  potenti sed id odio nunc. Molestie mauris morbi nunc malesuada varius elementum.
-                  Odio aenean adipiscing placerat tempor blandit pretium commodo. Eget sit aenean
-                  libero habitant mus ultricies lorem. Non vitae ante tempor diam amet nibh id orci
-                  in. Massa lacus aliquet sed suspendisse id. Cursus pulvinar integer eget justo
-                  accumsan nullam neque nulla enim. Commodo pretium habitasse varius laoreet viverra
-                  rutrum rhoncus feugiat. Faucibus elit laoreet nisi tellus aliquam in commodo.
-                  Consequat molestie blandit aliquam euismod enim arcu.
-                </p>
+                <div v-if="$i18n.locale === 'ar'">
+                  <p>
+                    من المتوقع أن يتم طرح أول صندوق نقد متداول للبيتكوين في أوروبا للعامة هذا الشهر،
+                    بعد تأخير 12 شهرًا عن موعد إطلاقه الذي كان مخططا له. حيث كانت قد أعلنت شركة
+                    Jacobi Asset Management سابقا أن ETF Bitcoin الخاص بها كان سيتم إدراجه في منصة
+                    Euronext Amsterdam في يوليو 2022.
+                  </p>
+                  <p>
+                    بعد أن قرروا في العام الماضي “اأن لوقت لم يكن مناسبًا” بعد انهيار عملة Terra
+                    Luna في مايو 2022 وانهيارمنصة FTX لاحقا في نوفمبر. تقول الشركة الآن أن الصندوق
+                    “على المسار الصحيح” لإطلاقه هذا الشهر، فالان حسبما يقول مدير الأصول لدى الشركة
+                    أن “الطلب قد تغير منذ الصيف الماضي”.
+                  </p>
+                  <p>
+                    في أوروبا ، كانت بورصة الأصول الرقمية الأوروبية قد نظمت المنتجات القابلة للتداول
+                    كسندات يتم تداولها في البورصة وليس كنقود حتى الآن.
+                  </p>
+                  <p>
+                    الفرق هنا أنه في حالة صناديق النقد المتداولة ETFs يمتلك كل مساهم جزءًا من الأسهم
+                    التي يمتلكها الصندوق، بينما في سندات النقد المتداولة ETNs يمتلك المستثمرون سندات
+                    الدين التي يتتبعها الصندوق، وليس أصولا يمتلكها الصندوق.
+                  </p>
+                  <p>
+                    وركزت الشركة بشكل كبير في إطلاقها على حقيقة أنهم سيطلقون ETF وليس ETN. حيث قام
+                    بيتر لين ، الشريك المؤسس والرئيس التنفيذي للعمليات في الشركة بالتصريح لـ Ignites
+                    Europe العام الماضي أن مصدري السندات كانوا مذنبين “بإساءة استخدام” مصطلح ETF.
+                  </p>
+                  <p>
+                    وقال أيضا: “كان هناك الكثير من المعلومات الخاطئة وسوء الاستخدام لمصطلح ETF من
+                    قبل مصدري الـ[ETN] ، ومن المحتمل أن ذلك يتم لإخفاء المخاطر الكامنة في الحصول على
+                    ETNs والاستثمار فيها”.
+                  </p>
+                  <p>
+                    وبحسب قول الشركة أن صندوق النقد المتداول الخاص بها، على عكس صناديق السندات
+                    المتداولة ETNs، لن تسمح باستخدام رافعة مالية أو استخدام المشتقات، وإلا ستؤدي
+                    بخلاف ذلك إلى “مخاطر كبيرة للطرف المقابل”.
+                  </p>
+                  <p>
+                    الصندوق تم ترخيصه في غيرنسي، حسبما صرح ديفيد كروسلاند الشريك في شركة المحاماة
+                    كاري أولسن، وهي ولاية قضائية تقدم “عددًا من المزايا” للمساعدة في إطلاق صندوق
+                    النقد المتداول للبيتكوين.
+                  </p>
+                  <p>
+                    وقال: ” غيرنسي بصفتها سلطة قضائية تختص في خدمة الصناديق ولا تخضع للعقبات
+                    المتأصلة بعضويتها في الاتحاد الأوروبي ، كانت قادرة على التحرك بسرعة للتكيف لدعم
+                    هذا الإطلاق”.
+                  </p>
+                  <p>
+                    وأضاف كروسلاند: “لقد سمحت مرونة [غيرنسي] واستعدادها لفهم التفاصيل المعقدة لمقدم
+                    العرض بإطلاق وسيلة تمويل تستفيد من حالة التنظيم في وقت لا تتمكن السلطات القضائية
+                    الأوروبية الأخرى من القيام بذلك”.
+                  </p>
+                  <p>
+                    قال مايكل أوريوردان ، الشريك المؤسس لمؤسسة التدريب الأوروبية وشركة بلاك ووتر
+                    لاستشارات الأصول الرقمية ، إن التحديات التنظيمية لإطلاق بيتكوين ETF في أوروبا
+                    كانت “كبيرة جدًا” حيث لم يتم اعتبار البيتكوين أحد الأصول المؤهلة بموجب قواعد
+                    Ucits.
+                  </p>
+                  <p>
+                    “ومع ذلك ، من الناحية العملية ، وبغض النظر عن بعض الاختلافات الهيكلية ، فإن ETF
+                    و ETP متشابهان تمامًا” ، كما قال.
+                  </p>
+                  <p>
+                    وأضاف أوريوردان: “كصناعة ، لم نقم بعمل جيد في التمييز بين كل منها ، ونتيجة لذلك
+                    هناك الكثير من الالتباس في السوق بشأن أيهما”.
+                  </p>
+                  <p>
+                    بلغ صافي التدفقات للأصول الرقمية الأوروبية على مدار الثمانية عشر شهرًا الماضية
+                    483 مليون دولار ، بما في ذلك التدفقات الداخلة البالغة 398 مليون دولار في الربع
+                    الثالث من عام 2022 وحده ، وفقًا لبيانات من Coinbase و Bloomberg. وبلغت قيمة
+                    الأصول الرقمية الأوروبية من المنتجات النقدية المتداولة 4.3 مليار يورو ، بعد أن
+                    بلغت ذروتها عند 10.5 مليار يورو في نهاية عام 2021 ، وفقًا لتحليل Ignites Europe
+                    لبيانات Morningstar.
+                  </p>
+                </div>
+                <div v-else>
+                  <p>
+                    It is expected that the first publicly available Bitcoin exchange-traded fund
+                    (ETF) in Europe will be launched this month, after a 12-month delay from its
+                    planned launch. Jacobi Asset Management had previously announced that its
+                    Bitcoin ETF would be listed on the Euronext Amsterdam platform in July 2022.
+                  </p>
+                  <p>
+                    After deciding last year that the "timing was not right" following the collapse
+                    of the Terra Luna currency in May 2022 and the subsequent collapse of the FTX
+                    platform in November, the company now says the fund is "on track" for a launch
+                    this month. According to the company's asset manager, "demand has changed since
+                    last summer."
+                  </p>
+                  <p>
+                    In Europe, the European Digital Assets Exchange had organized tradable products
+                    as bonds that are traded on the exchange and not as cash until now.
+                  </p>
+                  <p>
+                    The difference here is that in the case of ETFs, each shareholder owns a portion
+                    of the shares that the fund owns, while in exchange-traded notes (ETNs),
+                    investors own the debt securities tracked by the fund, not assets owned by the
+                    fund.
+                  </p>
+                  <p>
+                    The company has heavily focused its launch on the fact that they will be
+                    launching an ETF and not an ETN. Peter Lin, the co-founder and CEO of operations
+                    at the company, claimed to Ignites Europe last year that bond issuers were
+                    guilty of "misusing" the term ETF.
+                  </p>
+                  <p>
+                    He also said: "There was a lot of misinformation and misuse of the term ETF by
+                    [ETN] issuers, and it is likely that this is done to conceal the inherent risks
+                    in obtaining ETNs and investing in them."
+                  </p>
+                  <p>
+                    According to the company, its ETF, unlike ETNs, will not allow the use of
+                    leverage or derivatives, otherwise it would lead to "significant risks to the
+                    counterparty".
+                  </p>
+                  <p>
+                    The fund was licensed in Guernsey, according to David Crosland, a partner at law
+                    firm Carey Olsen, a jurisdiction that offers "a number of advantages" to help
+                    launch a Bitcoin ETF.
+                  </p>
+                  <p>
+                    He said: "As a jurisdiction specialized in fund services and not subject to the
+                    inherent obstacles of its membership in the European Union, Guernsey was able to
+                    move quickly to adapt to support this launch".
+                  </p>
+                  <p>
+                    Crosland added: "The flexibility of [Guernsey] and its readiness to understand
+                    the complex details of the issuer have enabled the launch of a funding mechanism
+                    that benefits from the regulatory status at a time when other European
+                    jurisdictions are unable to do so."
+                  </p>
+                  <p>
+                    Michael O'Riordan, co-founder of the European Training Foundation and Blackwater
+                    Digital Asset Consulting, said the regulatory challenges to launching a Bitcoin
+                    ETF in Europe were "very large" as Bitcoin was not considered an eligible asset
+                    under Ucits rules.
+                  </p>
+                  <p>
+                    "However, practically speaking, and regardless of some structural differences,
+                    ETF and ETP are virtually identical," he said.
+                  </p>
+                  <p>
+                    O'Riordan added: "As an industry, we have not done a good job distinguishing
+                    between the two, and as a result, there is a lot of confusion in the market
+                    about which is which."
+                  </p>
+                  <p>
+                    The net inflows for European digital assets over the past eighteen months
+                    reached $483 million, including inflows of $398 million in the third quarter of
+                    2022 alone, according to data from Coinbase and Bloomberg. The value of European
+                    digital assets from exchange-traded cash products was 4.3 billion euros, after
+                    peaking at 10.5 billion euros at the end of 2021, according to an Ignites Europe
+                    analysis of Morningstar data.
+                  </p>
+                </div>
               </div>
             </div>
           </b-col>
-          <b-col lg="4">
+          <!-- <b-col lg="4">
             <b-row>
               <b-col lg="12">
                 <div class="blog-details__like">
@@ -100,31 +209,32 @@
                 </div>
               </b-col>
             </b-row>
-          </b-col>
+          </b-col> -->
         </b-row>
       </b-container>
     </div>
   </DefaultLayout>
 </template>
 <script>
-import ArticleLike from "@/components/modules/blog/ArticleLike/index.vue"
-import ArticleTop from "@/components/modules/blog/ArticleTop/index.vue"
+// import ArticleLike from "@/components/modules/blog/ArticleLike/index.vue"
+// import ArticleTop from "@/components/modules/blog/ArticleTop/index.vue"
 import BreadCrumb from "@/components/Shared/BreadCrumb/index.vue"
 
 export default {
   components: {
-    ArticleLike,
-    ArticleTop,
+    // ArticleLike,
+    // ArticleTop,
     BreadCrumb
   },
   data() {
     return {
+      loading: true,
       articleId: this.$route.params.id,
       article: {},
       articlesAR: [
         {
           id: 1,
-          title: "هذا هو المكان الذي سيكون فيه عنوان مقالتنا أو عنوانها.",
+          title: "إطلاق أول صندوق نقد متداول (ETF) للبيتكوين في أوروبا بعد تأخير دام 12 شهرًا",
           date: "2022/11/21 07:13:51",
           tags: [
             {
@@ -140,7 +250,7 @@ export default {
               name: "Bitcoin"
             }
           ],
-          image: "01-blog-card.svg",
+          image: "bitcoin.png",
           content: "",
           articleLikeList: [
             {
@@ -229,7 +339,7 @@ export default {
               name: "Bitcoin"
             }
           ],
-          image: "02-blog-card.svg",
+          image: "bitcoin.png",
           content: "",
           articleLikeList: [
             {
@@ -943,7 +1053,7 @@ export default {
               name: "Bitcoin"
             }
           ],
-          image: "01-blog-card.svg",
+          image: "bitcoin.png",
           content: "",
           articleLikeList: [
             {
@@ -1730,6 +1840,9 @@ export default {
     }
   },
   mounted() {
+    setTimeout(() => {
+      this.loading = false
+    }, 1500)
     this.getArticleById(this.articleId)
     window.scrollTo({
       top: 0,
